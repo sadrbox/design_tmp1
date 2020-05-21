246cd1b5266e56995e35342ddb9d6ad433be3275
sadrbox <sadrbox@mail.ru>
2020-05-21 15:14:59 +0600

    fresh

---
+++ webpack.config.js
@@ -0,0 +1,63 @@
+const path = require("path");
+const webpack = require("webpack");
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+
+module.exports = {
+  entry: {
+    "js/script.min.js": "./static/assets/js/script.js",
+    "css/style.min.css": "./static/assets/scss/style.scss",
+  },
+  output: {
+    path: path.resolve(__dirname, "static"),
+    filename: "[name]",
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: "babel-loader",
+        options: {
+          presets: ["@babel/preset-env"],
+        },
+      },
+      {
+        test: /\.(css|scss)$/,
+        use: ExtractTextPlugin.extract({
+          fallback: "style-loader",
+          use: [
+            {
+              loader: "css-loader",
+            },
+            { loader: "postcss-loader" },
+            { loader: "sass-loader" },
+          ],
+        }),
+      },
+      {
+        test: /\.vue$/,
+        loader: "vue-loader",
+        options: {
+          loaders: {
+            scss: "vue-style-loader!css-loader!sass-loader",
+          },
+        },
+      },
+    ],
+  },
+  resolve: {
+    alias: {
+      vue$: "vue/dist/vue.esm.js",
+    },
+  },
+  plugins: [
+    new ExtractTextPlugin("[name]"),
+    new webpack.LoaderOptionsPlugin({
+      vue: {
+        loaders: {
+          scss: "style!css!scss",
+        },
+      },
+    }),
+  ],
+};
